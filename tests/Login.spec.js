dc0aa79ebf2d5844e62d8654220cc106ab3a8ad7
jaisurya <jaisuryarajasekhar@gmail.com>
2025-05-22 12:04:38 +0530

    Initial Commit

---
+++ tests/Login.spec.js
@@ -0,0 +1,12 @@
+import {test,expect} from '@playwright/test';
+import {Login} from '../pages/Login.js';
+
+test('Login Test',async ({page})=>{
+   const login=new Login(page);
+    await login.gotoLoginpage();
+    await page.waitForTimeout(2000);
+    await login.login("admin@archax.com","Test@123");
+    await page.waitForTimeout(2000);
+    await login.logbtn();
+    await page.waitForTimeout(2000);
+})
